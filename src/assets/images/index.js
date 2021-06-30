6307bfab19062f954d3c60e2e499f4eca71bb7c6
Renato Feitosa <pythaqua@gmail.com>
2021-06-30 20:15:06 -0300

    feat: Release beta version

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,17 +2,37 @@ import CoverImage from './cover.png';
 import CoverText from './cover-text.png';
 import Page1 from './pagina1.jpg';
 import Page1Text from './pagina1-texto1.png';
+import Page2 from './pagina2.jpg';
+import Page2Text from './pagina2-texto1.png';
 
 const images = {
   cover: { image: CoverImage, text: CoverText },
   pages: [
     {
-      order: 1,
       image: Page1,
       text: Page1Text,
     },
+    // {
+    //   image: Page2,
+    //   text: Page2Text,
+    // },
+    // {
+    //   image: Page2,
+    //   text: Page2Text,
+    // },
+    // {
+    //   image: Page2,
+    //   text: Page2Text,
+    // },
+    // {
+    //   image: Page2,
+    //   text: Page2Text,
+    // },
+    // {
+    //   image: Page2,
+    //   text: Page2Text,
+    // },
   ],
-  Page1,
 };
 
 export default images;
